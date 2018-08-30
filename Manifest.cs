d8426d2768339450ef2bb54a2b693967c88daf99
Marcos Aidos <Marcos@grupomadigital.onmicrosoft.com>
2018-08-30 10:30:49 +0100

    New age recipe added testing needed

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+﻿using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "NewAge Theme",
+    Author = "Marcos Aidos anf Sijmen Koffeman",
+    Website = "https://startbootstrap.com/template-overviews/new-age/",
+    Version = "1.0.0-beta3",
+    Description = "A theme adapted for landingpage website."
+)]
