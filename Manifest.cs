da7bc4b776a597dacc9fba578e0de2f4f21a2ebb
psijkof <koffemans@hotmail.com>
2018-08-30 16:21:04 +0100

    added config files for package deployment

--- Manifest.cs
+++ Manifest.cs
@@ -5,5 +5,5 @@
     Author = "Marcos Aidos anf Sijmen Koffeman",
     Website = "https://startbootstrap.com/template-overviews/new-age/",
     Version = "1.0.0-beta3",
-    Description = "A theme adapted for landingpage website."
+    Description = "NewAge theme adapted for Orchard Core."
 )]
